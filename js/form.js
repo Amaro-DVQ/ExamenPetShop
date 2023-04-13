6d0912c924284bba8dcb9f6b7144a94d340c4303
wiwoduoc <am.veas@duocuc.cl>
2023-04-12 23:47:58 -0400

    12042023n2

--- js/form.js
+++ js/form.js
@@ -4,12 +4,50 @@ document.getElementById("varCorreo").style.display = "none";
 document.getElementById("varPassord").style.display = "none";
 document.getElementById("varRPassword").style.display = "none";
 
+
+
 function validar() {
-    if (document.getElementById("form3Example1cg").value == "") {
+
+
+    if (document.getElementById("form3Example1cg").value.length == 0 ) {
         document.getElementById("varNombre").style.display = "inline";
         document.getElementById("form3Example1cg").classList.add("is-invalid");
     }else{
         document.getElementById("varNombre").style.display = "none";
         document.getElementById("form3Example1cg").classList.remove("is-invalid");
+        document.getElementById("form3Example1cg").classList.add("is-valid");   
+    }
+
+
+
+    if (document.getElementById("form3Example3cg").value.length == 0) {
+        document.getElementById("varCorreo").style.display = "inline";
+        document.getElementById("form3Example3cg").classList.add("is-invalid");
+    }else{
+        document.getElementById("varCorreo").style.display = "none";
+        document.getElementById("form3Example3cg").classList.remove("is-invalid");
+        document.getElementById("form3Example3cg").classList.add("is-valid");   
+
+    }
+
+
+
+    if (document.getElementById("form3Example4cg").value.length == 0) {
+        document.getElementById("varPassord").style.display = "inline";
+        document.getElementById("form3Example4cg").classList.add("is-invalid");
+    }else{
+        document.getElementById("varPassord").style.display = "none";
+        document.getElementById("form3Example4cg").classList.remove("is-invalid");
+        document.getElementById("form3Example4cg").classList.add("is-valid");   
+    }
+
+
+    if (document.getElementById("form3Example4cdg").value.length == 0) {
+        document.getElementById("varRPassword").style.display = "inline";
+        document.getElementById("form3Example4cdg").classList.add("is-invalid");
+    }else{
+        document.getElementById("varRPassword").style.display = "none";
+        document.getElementById("form3Example4cdg").classList.remove("is-invalid");
+        document.getElementById("form3Example4cdg").classList.add("is-valid");   
     }
 }
