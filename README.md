8f0b985f1bd1bebf181242dc1186a22e25f11670
Amaro-DVQ <am.veas@duocuc.cl>
2023-04-19 11:30:18 -0400

    1904(2)

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ Esta incompleto
 19-04
 
 Cambie el icono nomas
+
+19-04 (2)
+Agregue la wea del reloj , no me acuerdo si se hacia aqui o no pero lo deje comentao en el index todo
+
+Se ve feo y el responsive lo deja peor
--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ function login() {
     var password = document.getElementById("form3Example4cg").value;
     if (username == "admin" && password == "admin") {
         alert("Bienvenido!");
-    window.location = "index.html";
+    window.location = "indexSignup.html";
             } else {
         alert("Usuario o contraseña incorrectos");
             }
