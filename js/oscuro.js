4670f76c4b84e4b590f4e4ff1c6a561199924354
Amaro David Veas Quezada <118324921+Amaro-DVQ@users.noreply.github.com>
2023-05-10 00:20:00 -0400

    Add files via upload
    Signed-off-by: Amaro David Veas Quezada  <118324921+Amaro-DVQ@users.noreply.github.com>

---
+++ js/oscuro.js
@@ -0,0 +1,24 @@
+let toggle=document.getElementById('toggle');
+let moscuro=document.getElementById('moscuro');
+
+toggle.addEventListener('change',(event)=>{
+    let checked=event.target.checked;
+
+    document.body.classList.toggle('dark');
+
+
+
+
+
+
+    if(checked==true){
+        moscuro.innerHTML ='<i class="fa-solid fa-sun"></i>';   
+        moscuro.style.color = '#dfba14';
+    }else
+    {
+        moscuro.innerHTML ='<i class="fa-solid fa-moon"></i>';
+        moscuro.style.color = 'black';    
+    }
+
+
+})
